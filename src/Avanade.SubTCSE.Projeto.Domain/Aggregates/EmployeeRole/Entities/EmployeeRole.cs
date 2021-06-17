7a94df1f4966c0095ef61b4b3b817ba4902ac8bf
ramunizbh <ramunizbh@yahoo.com.br>
2021-06-17 08:59:33 -0300

    aula4

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
@@ -0,0 +1,19 @@
+﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Entities
+{
+    public class EmployeeRole
+    {
+        public EmployeeRole(string id, string roleName)
+        {
+            Id = id;
+
+            RoleName = roleName;
+        }
+
+
+        public string Id { get; set; }
+
+        public string RoleName { get; set; }
+
+
+    }
+}
